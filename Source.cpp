037b7061a774b4933368458cf66b156483c6bcd5
Jorge Padilla <43005165+Jorge-Padilla@users.noreply.github.com>
2021-05-30 23:02:51 -0500

    Done :)

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <iomanip>
 #include <stdint.h>
+#include <intrin.h>
 
 using namespace std;
 
@@ -14,7 +15,6 @@ void multiplicaDosMatrices(double** resultado, double** A, double** B, int verti
         for (int j = 0; j < horizontalResultado; j++) {
             *(*(resultado + i) + j) = 0;
             for (int k = 0; k < horizontalA; k++) {
-                //cout<< *(*(A+i)+k) << " * " << *(*(B+j)+k) << " = " << *(*(A+i)+k) * *(*(B+j)+k)<<endl;
                 *(*(resultado + i) + j) += (*(*(A + i) + k)) * (*(*(B + j) + k));
             }
         }
@@ -27,11 +27,30 @@ void multiplicaDosMatricesOMP(double** resultado, double** A, double** B, int ve
     for (int i = 0; i < verticalResultado; i++) {
         for (int j = 0; j < horizontalResultado; j++) {
             *(*(resultado + i) + j) = 0;
-//#pragma omp parallel for firstprivate(i, j)
             for (int k = 0; k < horizontalA; k++) {
-                //cout<< *(*(A+i)+k) << " * " << *(*(B+j)+k) << " = " << *(*(A+i)+k) * *(*(B+j)+k)<<endl;
                 *(*(resultado + i) + j) += *(*(A + i) + k) * *(*(B + j) + k);
             }
+        }
+    }
+}
+
+void multiplicaDosMatricesIntrin(double** resultado, double** A, double** B, int verticalResultado, int horizontalResultado, int verticalA, int horizontalA, int verticalB, int horizontalB) {
+    for (int i = 0; i < verticalResultado; i++) {
+        for (int j = 0; j < horizontalResultado; j++) {
+            *(*(resultado + i) + j) = 0;
+            __m256d a_reg, b_reg, c_reg;
+            double* aux;
+            aux = (double*)malloc(sizeof(double)*4);
+            for (int k = 0; k < horizontalA; k += 4) {
+
+                a_reg = _mm256_load_pd(*(A + i) + k);
+                b_reg = _mm256_load_pd(*(B + j) + k);
+                c_reg = _mm256_mul_pd(a_reg, b_reg);
+                _mm256_store_pd(aux, c_reg);
+
+                *(*(resultado + i) + j) += aux[0] + aux[1] + aux[2] + aux[3];
+            }
+            free(aux);
         }
     }
 }
@@ -64,11 +83,11 @@ int main() {
     ifstream lecturaA;
     ifstream lecturaB;
     ofstream archivoResultante;
-    //lecturaA.open("matrixA9.txt");
-    //lecturaB.open("matrixB9.txt");
+    //lecturaA.open("matrixA16.txt");
+    //lecturaB.open("matrixB16.txt");
     lecturaA.open("matrixA1048576.txt");
     lecturaB.open("matrixB1048576.txt");
-    //archivoResultante.open("matrizResultante3.txt");
+    //archivoResultante.open("matrizResultante4.txt");
     archivoResultante.open("matrizResultante1024.txt");
 
     int horizontalA, verticalA;
@@ -183,6 +202,22 @@ int main() {
     inicio = nanos();
 
     multiplicaDosMatricesOMP(resultado, matrizA, matrizB, verticalResultado, horizontalResultado, verticalA, horizontalA, verticalB, horizontalB);
+
+    fin = nanos();
+    end = clock();
+
+    tiempoTranscurrido = end - start;
+    tiempoEnNanosegundos = fin - inicio;
+
+    cout << "Se obtuvo el resultado en " << tiempoTranscurrido << " ms" << endl;
+    cout << "Se obtuvo el resultado en " << tiempoEnNanosegundos << " ns" << endl;
+    
+    cout << "-----Intrinsecas-----" << endl;
+
+    start = clock();
+    inicio = nanos();
+
+    multiplicaDosMatricesIntrin(resultado, matrizA, matrizB, verticalResultado, horizontalResultado, verticalA, horizontalA, verticalB, horizontalB);
 
     fin = nanos();
     end = clock();
